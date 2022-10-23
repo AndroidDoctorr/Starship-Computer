7f6f6b5ca9db75d256c470596ce5645284ce6099
AndroidDoctorr <gbartthenerd@gmail.com>
2022-10-23 16:40:15 -0400

    added PlasmaStream physical process

---
+++ Assets/Scripts/Physical Processes/PlasmaStream.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaStream : MonoBehaviour, IKineticProcess, IThermalProcess
+{
+    public double Temperature { get; }
+    public double Mdot { get; }
+    public double V { get; }
+    public double SigmaRadius { get; }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}

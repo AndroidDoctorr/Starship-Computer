a0ca352ce9b19f5700c2a3b15834ce385c60822f
AndroidDoctorr <gbartthenerd@gmail.com>
2022-10-24 01:41:45 -0400

    propagating event from process to sensor to system

--- Assets/Scripts/Computer/Devices/TemperatureSensor.cs
+++ Assets/Scripts/Computer/Devices/TemperatureSensor.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class TemperatureSensor : MonoBehaviour
 {
     public ThermalProcess ThermalProcess;
+    public delegate void TemperatureChangeDelegate(double newTemp);
+    public event TemperatureChangeDelegate OnTemperatureChange;
     void OnEnable()
     {
-        
+        ThermalProcess.OnTemperatureChange += UpdateTemp;
+    }
+    private void OnDestroy()
+    {
+        ThermalProcess.OnTemperatureChange -= UpdateTemp;
+    }
+    private void UpdateTemp(double newTemp)
+    {
+        OnTemperatureChange(newTemp);
     }
 }
--- Assets/Scripts/Physical Processes/ThermalProcess.cs
+++ Assets/Scripts/Physical Processes/ThermalProcess.cs
@@ -6,4 +6,5 @@ public class ThermalProcess : MonoBehaviour
 {
     public virtual double Temperature { get; }
     public delegate void TemperatureChangeDelegate(double newTemp);
+    public event TemperatureChangeDelegate OnTemperatureChange;
 }

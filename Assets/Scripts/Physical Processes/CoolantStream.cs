5fef3a8688c79f1093efe18ef947ab689c02a2c2
AndroidDoctorr <gbartthenerd@gmail.com>
2022-10-28 22:29:50 -0400

    updated coolant math

--- Assets/Scripts/Physical Processes/CoolantStream.cs
+++ Assets/Scripts/Physical Processes/CoolantStream.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class CoolantStream : KineticProcess
 {
     public AntimatterReaction AntimatterReaction;
+    private static double _CoolantCapacity = 50;
+    private static double _CoolantRate = 20;
+    private static double _CoolantDensity = 1;
+    private static double _PumpArea = 1;
+    private static double _ReactorCapacity = 10;
 
-    public override double Temperature { get { return AntimatterReaction.Temperature; } }
+    public override double Temperature =>
+        (AntimatterReaction.Temperature * _ReactorCapacity) / _CoolantCapacity;
     public override event TemperatureChangeDelegate OnTemperatureChange;
-    public override double Mdot { get { return AntimatterReaction.PlasmaRatePerEngine; } }
+    public override double Mdot => Temperature / _CoolantRate;
     public override event MdotChangeDelegate OnMdotChange;
-    public override double V { get { return AntimatterReaction.PlasmaV; } }
+    public override double V => Mdot / _CoolantDensity / _PumpArea;
     public override event VChangeDelegate OnVChange;
     void Update()
     {

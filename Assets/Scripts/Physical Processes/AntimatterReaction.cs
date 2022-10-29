88afb433129f1136db49a9f45960829679b59a7f
AndroidDoctorr <gbartthenerd@gmail.com>
2022-10-28 20:53:17 -0400

    efficiency calculations

--- Assets/Scripts/Physical Processes/AntimatterReaction.cs
+++ Assets/Scripts/Physical Processes/AntimatterReaction.cs
@@ -13,30 +13,45 @@ public class AntimatterReaction : ThermalProcess
     public static double Vmas = 0.0001;
     public static double Vint = 0.01;
     // Efficiency constants
-    public static double 
-
+    public static double Erad = 0.1;
+    public static double Econ = 0.1;
+    public static double Emas = 0.1;
+    public static double Eint = 0.1;
+    // Physical interactions
     public MatterStream MatterStream;
     public MatterStream AntimatterStream;
     public DilithiumMatrix DilithiumMatrix;
     public PlasmaStream PortPlasmaStream;
     public PlasmaStream StarboardPlasmaStream;
 
+    // Reaction properties
     public double IntermixRatio => MatterStream.FlowRate / AntimatterStream.FlowRate;
-    public double PlasmaRateTotal => MatterStream.FlowRate - AntimatterStream.FlowRate;
-    public double PlasmaRatePerEngine => PlasmaRateTotal / 2;
     public double TotalPower => 2 * AntimatterStream.FlowRate * Math.Pow(C, 2);
+    public double PlasmaQ => TotalPower * Efficiency;
     public double LossQ => TotalPower * (1 - Efficiency);
     // TODO: Come up with a better way to estimate temp
     public override double Temperature => LossQ / 1000 * Randomness;
     public override event TemperatureChangeDelegate OnTemperatureChange;
-    public double PlasmaQ => TotalPower * Efficiency;
+    // Output properties
+    public double PlasmaRateTotal => MatterStream.FlowRate - AntimatterStream.FlowRate;
+    public double PlasmaRatePerEngine => PlasmaRateTotal / 2;
     public double PlasmaV => Math.Sqrt(2 * PlasmaQ / PlasmaRateTotal);
 
-    // Efficiency calculations
-    public double Efficiency => 0.4;
+
+    // Efficiency Calculations
+    public double Efficiency => 
+        SigmaRadiusEffect * 
+        ConfinementEffect *
+        MassEffect *
+        IntermixEffect;
+    // Efficiency Contributions
+    public double SigmaRadiusEffect => 1 - Erad * MatterStream.SigmaRadius * AntimatterStream.SigmaRadius;
+    public double ConfinementEffect => 1 - Econ; // TODO: No confinement field property defined yet
+    public double MassEffect => 1 - Emas / (Math.Sqrt(AntimatterStream.Mdot) * Math.Sqrt(MatterStream.Mdot));
+    public double IntermixEffect => 1 - Erad / IntermixRatio;
 
 
-    // Variance calculations
+    // Variance Calculations
     public double Randomness => 1 + UnityEngine.Random.Range((float) -Variance, (float) Variance);
     public double Variance =>
         DilithiumCapacityVariance +

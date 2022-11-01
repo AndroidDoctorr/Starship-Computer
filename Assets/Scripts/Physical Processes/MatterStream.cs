8a5adb032733d9fc0636f4a63e6c9bc32f786ca1
AndroidDoctorr <gbartthenerd@gmail.com>
2022-10-31 21:07:27 -0400

    fixed font color of intermix ratio control label

--- Assets/Scripts/Physical Processes/MatterStream.cs
+++ Assets/Scripts/Physical Processes/MatterStream.cs
@@ -10,14 +10,29 @@ namespace Assets.Scripts.Computer.Systems.Warp_Core
 {
     public class MatterStream : KineticProcess
     {
+        // Physical Constants
+        // TODO: Get this from confinement coils eventually
+        private double _confinementBase = 5;
+        private double _confinementMax = 100;
+        // TODO: Get this from the Injector eventually (property of the hardware used)
+        private double _baseRadius = 0.1; // in femtometers?
+        // TODO: Get this from the Phase Coil subsystem eventually
+        private double _confinementSetting = 0.1;
+        // TODO: Get this from the Phase Coil subsystem eventually
+        private double _confinementFrequency = 0.3;
+
         public Injector Injector;
         public ConstrictionCoils Coils;
+        public Transform StreamStart;
+        public Transform StreamEnd;
 
-        public override double Temperature { get; }
+
+
+        public override double Temperature => 2000;
         public override event TemperatureChangeDelegate OnTemperatureChange;
-        public override double V { get { return (vFinal + vInitial) / 2; } }
+        public override double V => vFinal;
         public override event VChangeDelegate OnVChange;
-        public override double Mdot { get { return FlowRate; } }
+        public override double Mdot => FlowRate;
         public override event MdotChangeDelegate OnMdotChange;
 
         private ParticleSystem _ps;
@@ -27,7 +42,8 @@ namespace Assets.Scripts.Computer.Systems.Warp_Core
             if (_ps == null) Debug.LogError("Matter stream cannot find particle system!!");
         }
         // Mass flow rate (~1 g/s is typical?)
-        public double FlowRate {
+        public double FlowRate
+        {
             get
             {
                 return Injector.ActualFlowRate;
@@ -37,11 +53,20 @@ namespace Assets.Scripts.Computer.Systems.Warp_Core
         public double vInitial { get; }
         // Velocity at the reactor
         public double vFinal { get; }
-        // Confinement - radius that contains 1 sigma of plasma concentration
-        public override double SigmaRadius { get; }
+        // Confinement properties
+        public double Confinement => _confinementBase + (_confinementMax - _confinementBase) * _confinementSetting;
+        public double Amplitude => (_baseRadius / Math.Pow(Confinement, 2));
+        public override double SigmaRadius => Amplitude;
         public override event SigmaChangeDelegate OnSigmaChange;
+        // public double GetSigmaRadiusAt(Transform location)
+        // {
+        // double phi = 0.5; // Ratio of phase frequency
+        // double theta =  
+        // return Amplitude * Math.Sin();
+        // }
         // Density of the stream at the reactor
-        public double Density {
+        public double Density
+        {
             get
             {
                 double area = Mathf.PI * Math.Pow(SigmaRadius, 2);

2ec66debb87e77ec1e33e356295a4b5d5f3e89b4
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-03 22:40:36 -0400

    import statement for device class

--- Assets/Scripts/Computer/Devices/LightFixture.cs
+++ Assets/Scripts/Computer/Devices/LightFixture.cs
@@ -1,16 +1,15 @@
+using Assets.Scripts.Computer;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightFixture : MonoBehaviour
+public class LightFixture : Device
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         

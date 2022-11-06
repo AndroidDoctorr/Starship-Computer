507e7db5af9d9cab697d6aec21afe213670e4df6
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-06 18:59:31 -0500

    added Environment UI

---
+++ Assets/Scripts/Computer/UIs/EnvironmentUI.cs
@@ -0,0 +1,20 @@
+using Assets.Scripts;
+using Assets.Scripts.Computer.Systems.Environment;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentUI : GenericUI
+{
+    public Environment Environment;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

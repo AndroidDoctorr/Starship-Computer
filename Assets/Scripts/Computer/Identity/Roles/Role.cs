9f1870cb69a82dee13df15abc34af0d97b02be68
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-20 01:03:41 -0500

    created basic identity models

---
+++ Assets/Scripts/Computer/Identity/Roles/Role.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Computer.Identity
+{
+    public class Role
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Role Definition
+        public int DefaultPriority { get; }
+        // Whitelist
+        //   Networks
+        //   - Systems
+        //   -- Subsystems
+        //   --- Devices
+
+        // Blacklist
+        //   Networks
+        //   - Systems
+        //   -- Subsystems
+        //   --- Devices
+    }
+}

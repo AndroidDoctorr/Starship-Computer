7ba569d95b290685de17a73bf0ff0b7f57273401
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-29 19:55:33 -0500

    created humidity sensor device prefab and script

---
+++ Assets/Scripts/Computer/Devices/HumiditySensor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Atmosphere;
+
+public class HumiditySensor : MonoBehaviour
+{
+    public Atmosphere Atmosphere;
+    public delegate void HumidityChangeDelegate(double newHumidity);
+    public event HumidityChangeDelegate OnHumidityChange;
+    void OnEnable()
+    {
+        Atmosphere.OnHumidityChange += UpdateHumidity;
+    }
+    private void OnDestroy()
+    {
+        Atmosphere.OnHumidityChange -= UpdateHumidity;
+    }
+    private void UpdateHumidity(double newHumidity)
+    {
+        OnHumidityChange(newHumidity);
+    }
+}

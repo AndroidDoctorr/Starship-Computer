191ec5e1e95c28e0ba40a6c62be61c920c973b19
AndroidDoctorr <gbartthenerd@gmail.com>
2022-12-07 19:55:18 -0500

    added comments to lay out startup process

--- Assets/Scripts/Computer/Core/MainComputer.cs
+++ Assets/Scripts/Computer/Core/MainComputer.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.Computer.Core;
+using Assets.Scripts.Computer.Core.Configuration;
 using Assets.Scripts.Computer.Core.CoreModules;
 using System;
 using System.Collections.Generic;
@@ -30,10 +31,64 @@ namespace Assets.Scripts.Computer
         // ICollection<INetwork>
 
         public ShipSystem[] Systems;
+        public Config Config;
         public MainComputer()
         {
             // Startup
             // Read config file, start up system
+
+            // Get total Processing capacity
+            // For each unit
+            // - Check if active. Alert if not
+            // - Get device properties, add to repo here for control
+            // Get total Memory capacity
+            // For each unit
+            // - Check if active. Alert if not
+            // - Get device properties, add to repo here for control
+            // Get total Learning capacity
+            // For each unit
+            // - Check if active. Alert if not
+            // - Get device properties, add to repo here for control
+
+            // For each Network in config - { networks: [], ... }
+            // - Give each Network an address
+            // - Add Network to some repo here???
+            // - Go through system list
+
+            // For each System in Network
+            // - Attach system to network
+            // - Mark system in Master Systems List as assigned to a network somehow
+            // - Give each System an address
+            // - Set priority of system within network
+            // - Set system default properties
+            // - Set default Usages
+            // - What else???
+
+            // For each Subsystem in System
+            // - Give Subsystem an address
+            // - Set priority of subsystem within system
+            // - Set subsystem default properties
+            // - Set default Usages
+            // - Set up Devices
+            // - Set up IODevices
+
+            // For each Device in Subsystem
+            // - Give each Device an address
+            // - Set priority of device
+            // - Set device default properties
+            // - Set default Usages
+            // - Turn on if on by default
+
+            // For each IODevice in Subsystem
+            // - Give each IODevice an address
+            // - Set priority of IO device
+            // - Set default UI
+            //   OR show a default "no UI selected" graphic
+            // - Turn on if on by default
+            // - Set default UI settings???
+
+            // Check Master Systems List - any unused? Error message
+            // What else???
 
             var powerProfiles = new Dictionary<string, PowerProfile>();
             _powerAllocator = new PowerAllocator(PowerModules, powerProfiles);

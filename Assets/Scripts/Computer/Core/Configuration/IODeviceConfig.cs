33e2590cc61f2a81afac09a50381cf20211c8e36
AndroidDoctorr <gbartthenerd@gmail.com>
2022-12-08 21:56:38 -0500

    added UI ID to IODevice

---
+++ Assets/Scripts/Computer/Core/Configuration/IODeviceConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Computer.Core.Configuration
+{
+    [Serializable]
+    public class IODeviceConfig : DeviceConfig
+    {
+        public string DefaultUI;
+        public ConsoleType ConsoleType;
+    }
+}

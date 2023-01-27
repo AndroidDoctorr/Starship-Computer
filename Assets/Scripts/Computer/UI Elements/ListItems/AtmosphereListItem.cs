9bca3675e7c7e3104be814ce493e8ae310bd94db
AndroidDoctorr <gbartthenerd@gmail.com>
2023-01-27 01:59:07 -0500

    added atmosphere list item

---
+++ Assets/Scripts/Computer/UI Elements/ListItems/AtmosphereListItem.cs
@@ -0,0 +1,48 @@
+﻿using Assets.Scripts.Computer.Systems.Environment.SubSystems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Computer.UI_Elements.ListItems
+{
+    public class AtmosphereListItem : UIListItem
+    {
+        public TMP_Text Type;
+        public TMP_Text Temperature;
+        public TMP_Text Humidity;
+        public TMP_Text Count;
+        public override void Populate(ListItemData data)
+        {
+            if (data is not AtmosphereData)
+            {
+                Debug.LogError($"Data not formatted for Lighting Group item: {data.GetType().Name}");
+                return;
+            }
+
+            AtmosphereData atmosData = data as AtmosphereData;
+
+            Name.text = atmosData.Name;
+            Type.text = atmosData.Type;
+        }
+    }
+    public class AtmosphereData : ListItemData
+    {
+        public string Name;
+        public string Type;
+
+        public AtmosphereData(AtmosphereGroup atmos)
+        {
+            if (atmos == null)
+                return;
+
+            Name = atmos.Name;
+            Type = atmos.GetType().Name;
+        }
+    }
+}

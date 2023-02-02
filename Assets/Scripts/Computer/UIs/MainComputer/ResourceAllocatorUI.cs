992201f37c9fe62b93446eb4609b52512ec6178f
AndroidDoctorr <gbartthenerd@gmail.com>
2023-02-01 21:17:52 -0500

    switched positions of logic and memory sections of UI to match hardware positions

--- Assets/Scripts/Computer/UIs/MainComputer/ResourceAllocatorUI.cs
+++ Assets/Scripts/Computer/UIs/MainComputer/ResourceAllocatorUI.cs
@@ -15,14 +15,34 @@ public class ResourceAllocatorUI : GenericUI
     public UIProperty NMemoryTotal;
     public UIProperty MemoryDevices;
     public UIProperty MemoryUsage;
+    public UIProperty MemoryBufferCap;
+    public UIProperty MemoryBuffer;
+    public UIProperty MemoryPowerCap;
+    public UIProperty MemoryPower;
+    public UIProperty MemoryCache;
+    public UIProperty MemoryTemp;
+
     public UIProperty DLearningTotal;
     public UIProperty NLearningTotal;
     public UIProperty LearningDevices;
     public UIProperty LearningUsage;
+    public UIProperty LearningBufferCap;
+    public UIProperty LearningBuffer;
+    public UIProperty LearningPowerCap;
+    public UIProperty LearningPower;
+    public UIProperty LearningCache;
+    public UIProperty LearningTemp;
+
     public UIProperty DLogicTotal;
     public UIProperty QLogicTotal;
     public UIProperty LogicDevices;
     public UIProperty LogicUsage;
+    public UIProperty LogicBufferCap;
+    public UIProperty LogicBuffer;
+    public UIProperty LogicPowerCap;
+    public UIProperty LogicPower;
+    public UIProperty LogicCache;
+    public UIProperty LogicTemp;
     void Start()
     {
         // Device[] devices = Environment.GetDevices();

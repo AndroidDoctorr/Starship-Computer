d734252b7067c6f857d35e2d433c3d197430b1ca
AndroidDoctorr <gbartthenerd@gmail.com>
2023-02-04 16:48:49 -0500

    created CoreSystem class

---
+++ Assets/Scripts/Computer/Core/NetworkModules/CoreSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Computer.Core
+{
+    public class CoreSystem : SystemBase
+    {
+        public CoreSystem()
+        {
+            Priority = 0;
+        }
+    }
+}
